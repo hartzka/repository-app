3f220a339996c3a7395d45dac6bc9ef0ea347f5c
hartzka <kai.hartzell@gmail.com>
2020-12-17 18:00:10 +0200

    10.1-10.2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up Äppylä.js to start working on your app!</Text>
+      <Text>Open appula.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
